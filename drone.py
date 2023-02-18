d35f63150d402dbeef42547c5139bb637bcd3afa
Dev Patel <hifromdev@gmail.com>
2023-02-18 00:28:57 -0500

    Added drone.py and .gitignore to add virtualenv.

---
+++ drone.py
@@ -0,0 +1,9 @@
+from djitellopy import Tello
+import time
+
+tello = Tello()
+tello.connect()
+
+# tello.takeoff()
+# We have got our drone and it is functional. This is the file for drone-movement-related code.
+# tello.land()
